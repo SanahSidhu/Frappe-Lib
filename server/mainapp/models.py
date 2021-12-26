098217661e72fdb104ebcc47382dedc52286654f
sanahsidhu <sanaa.02.march@gmail.com>
2021-12-27 01:21:26 +0530

    code cleanup and comments

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -1,10 +1,20 @@
+from typing import Dict, List
 import sqlite3 as s
 import random
 import string
-from typing import Dict, List
+
 
 
 def lib_user(path: str):
+    """
+    Creates the admin table
+
+    Args:
+        path: path to database
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -15,6 +25,15 @@ def lib_user(path: str):
 
 
 def create_member(path: str):
+    """
+    Creates the member table
+
+    Args:
+        path: path to database
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -25,6 +44,15 @@ def create_member(path: str):
 
 
 def create_issue(path: str):
+    """
+    Creates the issues table
+
+    Args:
+        path: path to database
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -35,33 +63,43 @@ def create_issue(path: str):
 
 
 def generate_uid(path: str) -> str:
-    """Generates a unique user id
+    """
+    Generates a unique member id
+
     Args:
-        None
+        path: path to database
+
     Returns:
         str
     """
     conn = s.connect(path)
     cur = conn.cursor()
 
-    uid = "".join(
-        random.choice(
-            string.ascii_uppercase + string.ascii_lowercase + string.digits
-        )
-        for _ in range(16)
-    )
+    uid = "".join(random.choice
+            (string.ascii_uppercase + string.ascii_lowercase + string.digits)for _ in range(16))
 
     chk_uid = f"select Mem_id from member where Mem_id = '{uid}'"
+
     cur.execute(chk_uid)
     chk_uid_res = cur.fetchall()
 
-    if(len(chk_uid_res)) == 0:
+    if chk_uid_res is None:
         return uid
     else:
         uid = generate_uid()
 
 
 def new_mem(path: str, data: tuple):
+    """
+    Inserts data for a new member
+
+    Args:
+        path: path to database
+        data: tuple with member details
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -72,7 +110,16 @@ def new_mem(path: str, data: tuple):
 
 
 def mem_exists(path: str, mem_email: str) -> bool:
-    #check if member exists
+    """"
+    Checks if the member already exists
+
+    Args:
+        path: path to database
+        mem_email: member email id
+
+    Returns:
+        bool: True if member exists
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -88,7 +135,16 @@ def mem_exists(path: str, mem_email: str) -> bool:
 
 
 def book_available(path: str, isbn: str) -> bool:
-    #if book is not already issued to sm1 else
+    """
+    Checks if the book is already issued
+
+    Args:
+        path: path to database
+        isbn: isbn code of book
+
+    Returns:
+        bool: True if book is available
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -104,7 +160,16 @@ def book_available(path: str, isbn: str) -> bool:
 
 
 def check_debt(path: str, mem_email: str) -> bool:
-    #if mem debt < 500
+    """
+    Checks member's pending debt
+
+    Args:
+        path: path to database
+        mem_email: member email id
+
+    Returns:
+        bool: True if member debt > 500
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -120,7 +185,18 @@ def check_debt(path: str, mem_email: str) -> bool:
 
 
 def update_mem(path: str, isbn: str, mem_email: str, fee: int):
+    """
+    Updates the member table when a book is issued
 
+    Args:
+        path: path to database
+        isbn: isbn code of book
+        mem_email: member email id
+        fee: rent fee of book
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -139,6 +215,16 @@ def update_mem(path: str, isbn: str, mem_email: str, fee: int):
 
 
 def update_issue(path: str, data: tuple):
+    """
+    Updates issue table when a book is issued
+
+    Args:
+        path: path to database
+        data: tuple with issue details
+
+    Returns:
+        None
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -149,10 +235,24 @@ def update_issue(path: str, data: tuple):
 
 
 def update_return(path: str, isbn: str, mem_email: str, fee: int) -> bool:
+    """
+    Updates the member and issue table when a book is returned
+
+    Args:
+        path: path to database
+        isbn: isbn code of book
+        mem_email: member email id
+        fee: rent fee of book
+
+    Returns:
+        None
+    """
+
     conn = s.connect(path)
     cur = conn.cursor()
 
     chk_isbn = f"select ISBN from member where Email = '{mem_email}'"
+
     cur.execute(chk_isbn)
     chk_isbn_res = cur.fetchall()
 
@@ -161,30 +261,37 @@ def update_return(path: str, isbn: str, mem_email: str, fee: int) -> bool:
         return False
     else:
         isbn_li = chk_isbn_res[0][0].split(",")
+
         for book_isbn in isbn_li:
             if book_isbn == isbn:
+
                 isbn_li.remove(isbn)
                 updated_isbn = ','.join(isbn_li)
 
                 mem_return = f"update member set ISBN = '{updated_isbn}' where Email = '{mem_email}'"
-                cur.execute(mem_return)
-
                 mem_return = f"update member set Debt = Debt - {fee} where Email = '{mem_email}'"
-                cur.execute(mem_return)
-
                 issue_return = f"delete from issues where ISBN = '{isbn}'"
+               
+                cur.execute(mem_return)
+                cur.execute(mem_return)
                 cur.execute(issue_return)
 
                 conn.commit()
+
                 return True
 
     return False
 
 
 def get_email(path: str):
-    """Gets all user emails from table
+    """
+    Gets all user emails from table
+
     Args:
         path: Path to database
+    
+    Returns:
+        None
     """
 
     conn = s.connect(path)
@@ -199,27 +306,42 @@ def get_email(path: str):
 
 
 def chk_admin_exist(path: str, ad_email: str) -> bool:
-    """Checks if user exists in database
+    """
+    Checks if admin record exists
+
     Args:
         path: Path to database
-        email: User email id
+        ad_email: admin email id
+
     Returns:
-        bool
+        bool: True if admin record exists
     """
     conn = s.connect(path)
     cur = conn.cursor()
 
     chk_admin = f"select * from admin where Email='{ad_email}'"
+
     cur.execute(chk_admin)
     res = cur.fetchall()
 
-    if len(res) == 0:
+    if res is None:
         return False
     else:
         return True
 
 
 def chk_pass(path: str, ad_email: str, passw: str) -> bool:
+    """
+    Checks the password for admin login
+
+    Args:
+        path: path to database
+        ad_email: admin email id
+        passw: password entered in form
+
+    Returns:
+        bool: True if password is correct
+    """
     conn = s.connect(path)
     cur = conn.cursor()
 
@@ -234,10 +356,11 @@ def chk_pass(path: str, ad_email: str, passw: str) -> bool:
 
 
 def check_errors(data: Dict[str, List]) -> bool:
-    """Utility to check for errors in respones
+    """
+    Checks for errors in respones
 
     Args:
-        response (Dict[str]): response from API
+        data: response from API
 
     Returns:
         bool: True if errors exist
